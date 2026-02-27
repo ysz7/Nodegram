a39f12afdd2ef4f7ad77b3c1c324ce4da66477c9
Denys Zhodik <charredcrowofficial@gmail.com>
2026-02-27 18:56:08 +0100

    updated start_url for docs

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -52,7 +52,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <NotificationProvider>
         <AppContent />
       </NotificationProvider>
